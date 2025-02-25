d4b0a461dd993bc3322bd7d307d023d8e5e074a2
João Victor <156962782+jvictordev7@users.noreply.github.com>
2025-02-24 22:23:30 -0300

    Introdução ao Firebase: Conhecendo Banco de Dados, Configurando Firebase, Consumindo e Buscando Listas no Banco de Dados

---
+++ src/App.js
@@ -0,0 +1,117 @@
+
+import { useState } from 'react'
+import { db } from './firebaseConnection'
+import { doc , setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore'
+
+import './app.css';
+
+
+function App() {
+  const [titulo, setTitulo] = useState('')
+  const [autor, setAutor] = useState('')
+
+  const [posts, setPosts] = useState([])
+
+
+  async function handleAdd() {
+    // await setDoc(doc(collection(db, "posts"), "12345"), {
+    //   titulo: titulo,
+    //   autor: autor
+    // })
+    // .then(() => {
+    //   console.log('POST ADICIONADO COM SUCESSO!')
+    // }
+    // )
+    // .catch(() => {
+    //   console.log('ERRO AO ADICIONAR POST' + error)
+    // })
+
+
+    await addDoc(collection(db, "posts"), {
+      titulo: titulo,
+      autor: autor
+    })
+    .then(() => {
+      console.log('POST ADICIONADO COM SUCESSO!')
+      setAutor('')
+      setTitulo('')
+    }
+    )
+    .catch((error) => {
+      console.log('ERRO AO ADICIONAR POST' + error)
+    })
+
+  }
+
+  async function buscarPost() {
+    // const docRef = doc(db, "posts", "12345")
+    // await getDoc(docRef)
+    // .then((snapshot) => {
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
+    // })
+    // .catch((error) => {
+    //   console.log('ERRO AO BUSCAR POST' + error)
+    // })
+
+    const postsRef = collection(db, "posts")
+    await getDocs(postsRef)
+    .then((snapshot) => {
+      let lista = []
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor
+        })
+      })
+      setPosts(lista)
+    })
+    .catch((error) => {
+      console.log('ERRO AO BUSCAR POST' + error)
+    })
+
+  }
+
+  return (
+    <div className="App">
+      <h1>React Js + Firebase :)</h1>
+
+      <div className='container'>
+        <label>Titulo:</label>
+        <textarea
+        type="text"
+        placeholder='Digite o titulo' 
+        value={titulo}
+        onChange={ (e) => setTitulo(e.target.value)}
+        />
+
+        <label>Autor:</label>
+        <input 
+        type="text"
+        placeholder='Autor do post'
+        value={autor}
+        onChange={ (e) => setAutor(e.target.value)}
+        />
+
+        <button onClick={handleAdd} >Adicionar Post</button>
+        <button onClick={buscarPost}>Buscar post</button>
+
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <p>Titulo: {post.titulo}</p>
+                <p>Autor: {post.autor}</p>
+              </li>
+            )})
+          }
+        </ul>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebaseConnection.js
@@ -0,0 +1,20 @@
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyADg086_21AUEE_P6bCFTtC2e2YFf1ILQQ",
+    authDomain: "cursoapp-237ae.firebaseapp.com",
+    projectId: "cursoapp-237ae",
+    storageBucket: "cursoapp-237ae.firebasestorage.app",
+    messagingSenderId: "497017217280",
+    appId: "1:497017217280:web:8d0239aa3b2394fa5a13e5",
+    measurementId: "G-7E3DETD1C2"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const db = getFirestore(firebaseApp);
+
+  export {db};
