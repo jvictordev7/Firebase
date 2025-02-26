5a59ff87f6cef400787eb9465a320e6cf550ce05
João Victor <156962782+jvictordev7@users.noreply.github.com>
2025-02-25 22:40:06 -0300

    Banco de Dados: Implementação de funções para criação, deleção de itens e monitoramento do banco

--- src/App.js
+++ src/App.js
@@ -1,133 +1,183 @@
-
-import { useState } from 'react'
-import { db } from './firebaseConnection'
-import { doc , setDoc, collection, addDoc, getDoc, getDocs, updateDoc } from 'firebase/firestore'
+import { useState, useEffect } from 'react'
+import { db } from './firebaseConnection';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc, onSnapshot } from 'firebase/firestore'
 
 import './app.css';
 
-
 function App() {
-  const [titulo, setTitulo] = useState('')
-  const [autor, setAutor] = useState('')
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
   const [idPost, setIdPost] = useState('')
 
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+
+  useEffect (()=> {
+    async function loadPosts(){
+      const unsub = onSnapshot(collection(db, "posts"), (snapshot) => {
+        let listaPost = []
+
+        snapshot.forEach((doc) => {
+          listaPost.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor
+          })
+        })
+
+        setPosts(listaPost)
+      })
+    }
+
+    loadPosts();
+  }, [])
 
 
-  async function handleAdd() {
-    // await setDoc(doc(collection(db, "posts"), "12345"), {
+  async function handleAdd(){
+    // await setDoc(doc(db, "posts", "12345"), {
     //   titulo: titulo,
-    //   autor: autor
+    //   autor: autor,
     // })
     // .then(() => {
-    //   console.log('POST ADICIONADO COM SUCESSO!')
-    // }
-    // )
-    // .catch(() => {
-    //   console.log('ERRO AO ADICIONAR POST' + error)
+    //   console.log("DADOS REGISTRADO NO BANCO!")
     // })
+    // .catch((error) => {
+    //   console.log("GEROU ERRO" + error)
+    // }) 
 
 
     await addDoc(collection(db, "posts"), {
       titulo: titulo,
-      autor: autor
+      autor: autor,
     })
     .then(() => {
-      console.log('POST ADICIONADO COM SUCESSO!')
-      setAutor('')
+      console.log("CADASTRADO COM SUCESSO")
+      setAutor('');
       setTitulo('')
-    }
-    )
-    .catch((error) => {
-      console.log('ERRO AO ADICIONAR POST' + error)
     })
+    .catch((error) => {
+      console.log("ERRO " + error)
+    })
+
 
   }
 
-  async function buscarPost() {
-    // const docRef = doc(db, "posts", "12345")
-    // await getDoc(docRef)
+
+  async function buscarPost(){
+    // const postRef = doc(db, "posts", "vFvZAyFqebXFsFv0X89l")
+    // await getDoc(postRef)
     // .then((snapshot) => {
     //   setAutor(snapshot.data().autor)
     //   setTitulo(snapshot.data().titulo)
+
     // })
-    // .catch((error) => {
-    //   console.log('ERRO AO BUSCAR POST' + error)
+    // .catch(()=>{
+    //   console.log("ERRO AO BUSCAR")
     // })
 
     const postsRef = collection(db, "posts")
     await getDocs(postsRef)
     .then((snapshot) => {
-      let lista = []
+      let lista = [];
+
       snapshot.forEach((doc) => {
         lista.push({
           id: doc.id,
           titulo: doc.data().titulo,
-          autor: doc.data().autor
+          autor: doc.data().autor,
         })
       })
-      setPosts(lista)
+
+      setPosts(lista);
+
     })
     .catch((error) => {
-      console.log('ERRO AO BUSCAR POST' + error)
+      console.log("DEU ALGUM ERRO AO BUSCAR")
     })
 
+
   }
 
-  function atualizarPost() {
-    alert('TESTE')
+
+  async function editarPost(){
+    const docRef = doc(db, "posts", idPost)
+    
+    await updateDoc(docRef, {
+      titulo: titulo,
+      autor: autor
+    })
+    .then(() => {
+      console.log("POST ATUALIZADO!")
+      setIdPost('')
+      setTitulo('')
+      setAutor('')
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+
   }
+
+    async function excluirPost(id) {
+      const docRef = doc(db, "posts", id)
+      await deleteDoc(docRef)
+      .then(() => {
+        console.log("POST DELETADO")
+      })
+      .catch((error) => {
+        console.log("ERRO AO DELETAR" + error)
+      })
+    } 
 
   return (
-    <div className="App">
-      <h1>React Js + Firebase :)</h1>
+    <div>
+      <h1>ReactJS + Firebase :)</h1>
 
-      <div className='container'>
+    <div className="container">
 
-        <label>ID do post:</label>
-        <input 
+      <label>ID do Post:</label>
+      <input
         placeholder='Digite o ID do post'
         value={idPost}
-        onChange={ (e) => setIdPost(e.target.value)}
-        /> <br />
+        onChange={ (e) => setIdPost(e.target.value) }
+      /> <br/>
 
-
-
-        <label>Titulo:</label>
-        <textarea
+      <label>Titulo:</label>
+      <textarea 
         type="text"
-        placeholder='Digite o titulo' 
+        placeholder='Digite o titulo'
         value={titulo}
-        onChange={ (e) => setTitulo(e.target.value)}
-        />
+        onChange={ (e) => setTitulo(e.target.value) }
+      />
 
-        <label>Autor:</label>
-        <input 
-        type="text"
-        placeholder='Autor do post'
+      <label>Autor:</label>
+      <input 
+        type="text" 
+        placeholder="Autor do post"
         value={autor}
-        onChange={ (e) => setAutor(e.target.value)}
-        />
+        onChange={(e) => setAutor(e.target.value) }
+      />
 
-        <button onClick={handleAdd} >Adicionar Post</button>
-        <button onClick={buscarPost}>Buscar post</button> <br />
+      <button onClick={handleAdd}>Cadastrar</button>
+      <button onClick={buscarPost}>Buscar post</button> <br/>
 
-        <button onClick={atualizarPost}>Atualizar Post</button>
+      <button onClick={editarPost}>Atualizar post</button>
 
 
-        <ul>
-          {posts.map((post) => {
-            return (
-              <li key={post.id}>
-                <strong>ID: {post.id}</strong> <br />
-                <p>Titulo: {post.titulo}</p>
-                <p>Autor: {post.autor}</p>
-              </li>
-            )})
-          }
-        </ul>
+      <ul>
+        {posts.map( (post) => {
+          return(
+            <li key={post.id}>
+              <strong>ID: {post.id}</strong> <br/>
+              <span>Titulo: {post.titulo} </span> <br/>
+              <span>Autor: {post.autor}</span> <br/> 
+              <button onClick={() => excluirPost(post.id)}>Excluir</button> <br/>  <br/> 
+            </li>
+          )
+        })}
+      </ul>
 
-      </div>
+    </div>
 
     </div>
   );
